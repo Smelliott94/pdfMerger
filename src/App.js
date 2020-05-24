c8058bd432488fc8fae4a76dda059607ddd01734
marcus elliott <marcuselliott94@gmail.com>
2020-05-24 18:56:04 +0200

    initial - Basic UI with file dragging and re-ordering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Button from 'react-bootstrap/Button'
+import { ArrowUp, ArrowDown}  from 'react-bootstrap-icons'
+
 
 function App() {
+
+  const [files, setFiles] = useState([])
+
+  const ondrop = (e) => {
+    e.preventDefault();
+    setFiles([...e.dataTransfer.files]);
+    Array.from(e.dataTransfer.files).forEach(f => {
+      console.log(f);
+    });
+  }
+  
+  const ondragover = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+  
+  const moveup = (index) => {
+    if (index < 1) return;
+    var newFiles = [...files];
+    var file = files[index];
+    newFiles.splice(index, 1);
+    newFiles.splice(index-1, 0, file);
+    setFiles(newFiles);
+  }
+
+  const movedown = (index) => {
+    if (files.length === index + 1) return;
+    var newFiles = [...files];
+    var file = files[index];
+    newFiles.splice(index, 1);
+    newFiles.splice(index+1, 0, file);
+    setFiles(newFiles);
+  }
+
+  const FileList = () => (
+    <div>
+      {files.map((file, i) =>
+        <p key={i}>{file.name} <Button onClick={ () => (moveup(i)) }><ArrowUp /></Button> <Button onClick={ () => (movedown(i)) }><ArrowDown /></Button> </p>
+      )}
+    </div>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="drag-file" onDrop={ondrop} onDragOver={ondragover}>
+        Drag your files here
+      </div>
+      <FileList />
     </div>
   );
 }
